54dc30609487d3d51a27412a07ba966fc7c38439
ArtemTkachev <art599@yandex.ru>
2017-09-24 18:47:02 +0300

    task 4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -13,7 +13,19 @@ public class Calculate{
 * @param args - args
 */
 	public static void main (String[] args){
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 	}
+
+
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
+
+
 }
 
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+* @author atkachev
+* @since 24.09.2017
+* @version 1
+*/
+public class CalculateTest{
+
+/**
+* Test echo.
+*/
+	@Test
+	public void whenTakeNameThenTreeEchoPlusName (){
+	String input = "atkachev";
+	String expect = "Echo, echo, echo : atkachev";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result, is(expect));
+	}
+
+
+}
+
