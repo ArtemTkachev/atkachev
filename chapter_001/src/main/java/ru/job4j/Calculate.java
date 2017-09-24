dc5543b18c914bb6407c66fdf61a7d1e184ee0b2
ArtemTkachev <art599@yandex.ru>
2017-09-24 14:02:35 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Класс для вывода информации.
+* @author atkachev
+* @since 24.09.2017
+* @version 1
+*/
 public class Calculate{
+
+/**
+* Вывод строки в консоль
+* @param args - args
+*/
 	public static void main (String[] args){
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package for calculate task
+*
+*@author atkachev (mailto:art599@yandex.ru)
+*@version 1
+*@since 24.09.2017
+*/
+package ru.job4j;
