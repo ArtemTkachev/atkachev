33489cdd296f9209fa59e87dd6f5bc51ae2f5f42
ArtemTkachev <art599@yandex.ru>
2017-09-28 00:10:11 +0300

    task 190

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -5,11 +5,18 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
+    Max maximum = new Max();
     @Test
     public void whenFirstLessSecond() {
-        Max maximum = new Max();
         int res = maximum.max(2,3);
         assertThat(res,is(3));
+
+    }
+
+    @Test
+    public void whenFirstLessSecondLessThird() {
+        int res = maximum.maxthreeparam(1,2,5);
+        assertThat(res,is(5));
 
     }
 }
