f7fe2990379fabc317ec6ee4a950e5e733a62d8b
ArtemTkachev <art599@yandex.ru>
2017-09-28 22:31:44 +0300

    task 9461

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -13,11 +13,11 @@ public class Triangle {
     }
 
     public double distance(Point left, Point right) {
-        return (Math.sqrt(Math.pow(right.getX() - left.getX(),2) + Math.pow(right.getY() - left.getY(),2)));
+        return (Math.sqrt(Math.pow(right.getX() - left.getX(), 2) + Math.pow(right.getY() - left.getY(), 2)));
     }
 
     public double perimeterh(double ac, double bc, double ab) {
-        return (ac + bc + ab)/2;
+        return (ac + bc + ab) / 2;
     }
 
     public double area() {
@@ -26,17 +26,15 @@ public class Triangle {
         double ac = this.distance(this.a, this.c);
         double bc = this.distance(this.b, this.c);
         double p = this.perimeterh(ac, bc, ab);
-        if (this.exist(ab,ac,bc)) {
-            rsl = Math.sqrt(p *(p - ab) * (p - ac) * (p - bc));
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
         }
         return rsl;
     }
 
     private boolean exist(double ab, double ac, double bc) {
-        if (ab+ac>bc && ab+bc>ac && ac+bc>ab)
-            return true;
-        else
-            return false;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
+
     }
 
 }
