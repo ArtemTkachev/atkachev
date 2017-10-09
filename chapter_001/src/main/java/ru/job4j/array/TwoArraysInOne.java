2a28dbc253a1ed57e3663632d2f5ed91e9920d2c
ArtemTkachev <art599@yandex.ru>
2017-10-09 20:38:58 +0300

    TwoArraysInOne_01

--- chapter_001/src/main/java/ru/job4j/array/TwoArraysInOne.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoArraysInOne.java
@@ -4,28 +4,32 @@ public class TwoArraysInOne {
 
     public int[] retArray(int[] firstarray, int[] secondarray) {
         int[] resultarray = new int[firstarray.length + secondarray.length];
-        int i =0;
-        int y =0;
-        for(int cnt = 0; cnt<resultarray.length; cnt++ ) {
-            if (i<firstarray.length && y<secondarray.length) {
-                if(firstarray[i]<secondarray[y]) {
-                    resultarray[cnt]=firstarray[i];
+        int i = 0;
+        int y = 0;
+        for (int cnt = 0; cnt < resultarray.length; cnt++) {
+            if (i < firstarray.length && y < secondarray.length) {
+                if (firstarray[i] < secondarray[y]) {
+                    resultarray[cnt] = firstarray[i];
                     i++;
-                }
-                else {
-                    resultarray[cnt]=secondarray[y];
+                } else {
+                    resultarray[cnt] = secondarray[y];
                     y++;
                 }
-            }
-            else if(i>=firstarray.length) {
+            } else if (i >= firstarray.length) {
+                /*
                 resultarray[cnt]=secondarray[y];
                 y++;
-            }
-            else if(y>=secondarray.length) {
+                */
+                System.arraycopy(secondarray, y, resultarray, cnt, secondarray.length-y );
+                break;
+            } else if (y >= secondarray.length) {
+               /*
                 resultarray[cnt]=firstarray[i];
                 i++;
+                 */
+                System.arraycopy(firstarray, i, resultarray, cnt, firstarray.length-i );
+                break;
             }
-
         }
 
         return resultarray;
