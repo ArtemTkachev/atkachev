253119e2c3f9db59151d5253299f5e602907adeb
ArtemTkachev <art599@yandex.ru>
2017-10-17 14:38:37 +0300

    task 10022.01

--- chapter_003/src/main/java/ru/job4j/CollectionsWork.java
+++ chapter_003/src/main/java/ru/job4j/CollectionsWork.java
@@ -19,7 +19,8 @@ public class CollectionsWork {
     private static final String mCHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private static final int STR_LENGTH = 9; // длина генерируемой строки
     private Random random = new Random();
- //   private String[] arrayMain;
+
+    //   private String[] arrayMain;
 /*
     public CollectionsWork(int amount) {
         this.arrayMain = new String[amount];
@@ -34,28 +35,47 @@ public class CollectionsWork {
         List<String> arrayList = new ArrayList<String>();
         List<String> linkedList = new LinkedList<String>();
         Set<String> treeSet = new TreeSet<String>();
-        long rtarlistadd = this.add(arrayList, 1000000);
-        long rtarlistdel = this.delete(arrayList, 1000000);
-        long rtlnklistadd = this.add(linkedList, 1000000);
-        long rtlnklistdel = this.delete(linkedList, 1000000);
-        long rtsetadd = this.add(treeSet, 1000000);
-        long rtsetdel = this.delete(treeSet, 1000000);
+        long[] rtarlistadds = new long[5];
+        long[] rtarlistdels = new long[5];
+        long[] rtlnklistadds = new long[5];
+        long[] rtlnklistdels = new long[5];
+        long[] rtsetadds = new long[5];
+        long[] rtsetdels = new long[5];
+
+        long rtarlistadd = 0, rtarlistdel = 0, rtlnklistadd = 0, rtlnklistdel = 0, rtsetadd = 0, rtsetdel = 0;
+
+        for (int z = 0; z < 5; z++) {
+            rtarlistadds[z] = this.add(arrayList, 1000000);
+            rtarlistdels[z] = this.delete(arrayList, 1000000);
+            rtlnklistadds[z] = this.add(linkedList, 1000000);
+            rtlnklistdels[z] = this.delete(linkedList, 1000000);
+            rtsetadds[z] = this.add(treeSet, 1000000);
+            rtsetdels[z] = this.delete(treeSet, 1000000);
+            rtarlistadd = rtarlistadd + rtarlistadds[z];
+            rtarlistdel = rtarlistdel + rtarlistdels[z];
+            rtlnklistadd = rtlnklistadd + rtlnklistadds[z];
+            rtlnklistdel = rtlnklistdel + rtlnklistdels[z];
+            rtsetadd = rtsetadd + rtsetadds[z];
+            rtsetdel = rtsetdel + rtsetdels[z];
+        }
+
         System.out.println(String.format("Время добавления:\n arrayList: %s \n LinkedList: %s \n TreeSet: %s ",
-                String.valueOf(rtarlistadd), String.valueOf(rtlnklistadd), String.valueOf(rtsetadd)));
+                String.valueOf(rtarlistadd/5), String.valueOf(rtlnklistadd/5), String.valueOf(rtsetadd/5)));
         System.out.println(String.format("Время удаления:\n arrayList: %s \n LinkedList: %s \n TreeSet: %s ",
-                String.valueOf(rtarlistdel), String.valueOf(rtlnklistdel), String.valueOf(rtsetdel)));
+                String.valueOf(rtarlistdel/5), String.valueOf(rtlnklistdel/5), String.valueOf(rtsetdel/5)));
 
     }
-/*
-    private String[] ar() {
-        String[] retar = new String[CollectionsWork.AMOUNT];
-        for (int i = 0; i < retar.length; i++) {
-            char ch = CollectionsWork.mCHAR.charAt(random.nextInt(CollectionsWork.mCHAR.length()));
-            retar[i] = String.valueOf(ch);
+
+    /*
+        private String[] ar() {
+            String[] retar = new String[CollectionsWork.AMOUNT];
+            for (int i = 0; i < retar.length; i++) {
+                char ch = CollectionsWork.mCHAR.charAt(random.nextInt(CollectionsWork.mCHAR.length()));
+                retar[i] = String.valueOf(ch);
+            }
+            return retar;
         }
-        return retar;
-    }
-*/
+    */
     private long add(Collection<String> collection, int amount) {
         long runtimestart = System.currentTimeMillis();
         for (int i = 0; i < amount; i++) {
