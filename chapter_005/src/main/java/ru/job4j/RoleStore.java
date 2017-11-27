19dbc6dee28ce147a23f2911199b15a1f773fd8d
ArtemTkachev <art599@yandex.ru>
2017-11-27 18:33:22 +0300

    task 157.02

--- chapter_005/src/main/java/ru/job4j/RoleStore.java
+++ chapter_005/src/main/java/ru/job4j/RoleStore.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class RoleStore extends AbstractStore<Role> {
-    private int size;
-    private SimpleArray<Role> simpleArray;
+
     public RoleStore(int size) {
         super(size);
     }
--- chapter_005/src/main/java/ru/job4j/UserStore.java
+++ chapter_005/src/main/java/ru/job4j/UserStore.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class UserStore extends AbstractStore<User> {
-    private int size;
-    private SimpleArray<User> simpleArray;
+
     public UserStore(int size) {
         super(size);
     }
