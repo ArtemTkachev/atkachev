65b38d8b0f36a654c726128ad24e148aa177e6a2
ArtemTkachev <art599@yandex.ru>
2017-12-13 14:38:02 +0300

    task 1005

--- chapter_005/src/main/java/ru/job4j/TwoSameObjects.java
+++ chapter_005/src/main/java/ru/job4j/TwoSameObjects.java
@@ -5,8 +5,9 @@ import java.util.*;
 public class TwoSameObjects {
 
     public void createTO() {
-        UserM user1 = new UserM("Игорь",0, new GregorianCalendar());
-        UserM user2 = new UserM("Игорь",0, new GregorianCalendar());
+        Calendar calendar = new GregorianCalendar();
+        UserM user1 = new UserM("Игорь",0, calendar);
+        UserM user2 = new UserM("Игорь",0, calendar);
         Map<UserM,Object> userObjectMap = new HashMap<>();
         userObjectMap.put(user1,1);
         userObjectMap.put(user2,2);
