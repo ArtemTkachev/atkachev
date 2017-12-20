13d63d5102cef2ea1496bacab944626cd62ec090
ArtemTkachev <art599@yandex.ru>
2017-12-20 21:54:55 +0300

    task 1008

--- chapter_005/src/main/java/ru/job4j/SimpleHashMap.java
+++ chapter_005/src/main/java/ru/job4j/SimpleHashMap.java
@@ -5,63 +5,108 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SimpleHashMap<K, V> implements SimpleMapContainer<K, V> {
-    private Object[] container = new Object[10];
+    private Object[] container = new Object[12];
+    private int count = 0;
 
 
     private class Ent<K, V> {
         private K key;
         private V value;
+        private int hash;
 
-        public Ent(K key, V value) {
+        public Ent(K key, V value, int hash) {
             this.key = key;
             this.value = value;
+            this.hash = hash;
         }
     }
 
     @Override
     public boolean insert(K key, V value) {
         boolean result = false;
-        int hashV = hash(key);
-        int cnrns = contains(key, hashV);
-        if (cnrns == 1) {
-            Ent<K, V> ent = (Ent<K, V>) container[hashV];
-            ent.value = value;
-            result = true;
-        } else if (cnrns == -1) {
-            while (hashV >= container.length) {
-                arrayUp();
+        if (key != null) {
+            int hcode = hash(key.hashCode());
+            int index = indexFor(hcode, container.length);
+            int cntns = contains(key, index);
+            if (cntns == 1) {
+                changeOldKey(index, value);
+                result = true;
+            } else if (cntns == -1) {
+                if (0.5 * container.length <= count) {
+                    int oldLen = container.length;
+                    arrayUp();
+                    transfer(oldLen);
+                    index = indexFor(hash(key.hashCode()), container.length);
+                    cntns = contains(key, index);
+                    if(cntns == -1) {
+                        createNewKey(index, key, value, hcode);
+                    }
+                } else {
+                    createNewKey(index, key, value, hcode);
+                }
+                result = true;
             }
-            container[hashV] = new Ent<>(key, value);
-            result = true;
         }
         return result;
     }
+
+    private void changeOldKey(int indx, V value) {
+        Ent<K, V> ent = (Ent<K, V>) container[indx];
+        ent.value = value;
+    }
+
+    private void createNewKey(int indx, K key, V value, int hash) {
+        container[indx] = new Ent<>(key, value, hash);
+        count++;
+    }
+
+    private void transfer(int oldLen) {
+        Object[] containerTemp = new Object[container.length];
+        for (int i = 0; i < oldLen; i++) {
+            if (container[i] != null) {
+                Ent<K, V> ent = (Ent<K, V>) container[i];
+                int index = indexFor(ent.hash, container.length);
+                if (containerTemp[index]!=null)
+                    count--;
+                containerTemp[index] = ent;
+            }
+        }
+        container = Arrays.copyOf(containerTemp, containerTemp.length);
+    }
+
 
     private void arrayUp() {
         container = Arrays.copyOf(container, container.length * 2);
     }
 
-    private int hash(K key) {
-        return Math.abs(key.hashCode() % container.length);
+    private int hash(int h) {
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
+    }
+
+    private int indexFor(int h, int length) {
+        return h & (length - 1);
     }
 
     @Override
     public V get(K key) {
         V result = null;
-        int hashV = hash(key);
-        if (contains(key, hashV) == 1) {
-            Ent<K, V> ent = (Ent<K, V>) container[hashV];
-            result = ent.value;
+        if (key != null) {
+            int index = indexFor(hash(key.hashCode()), container.length);
+            if (contains(key, index) == 1) {
+                Ent<K, V> ent = (Ent<K, V>) container[index];
+                result = ent.value;
+            }
         }
         return result;
     }
 
-    private int contains(K key, int hashVal) {
+    private int contains(K key, int indx) {
         int result = -1;
-        int hashV = hashVal != -1 ? hashVal : hash(key);
-        if (hashV < container.length) {
-            if (container[hashV] != null) {
-                Ent<K, V> ent = (Ent<K, V>) container[hashV];
+        int index = indx != -1 ? indx : indexFor(hash(key.hashCode()), container.length);
+        if (index < container.length) {
+            if (container[index] != null) {
+                Ent<K, V> ent = (Ent<K, V>) container[index];
                 if (ent.key.equals(key)) {
                     result = 1;
                 } else {
@@ -75,10 +120,13 @@ public class SimpleHashMap<K, V> implements SimpleMapContainer<K, V> {
     @Override
     public boolean delete(K key) {
         boolean result = false;
-        int hashV = hash(key);
-        if (contains(key, hashV) == 1) {
-            container[hashV] = null;
-            result = true;
+        if (key != null) {
+            int index = indexFor(hash(key.hashCode()), container.length);
+            if (contains(key, index) == 1) {
+                container[index] = null;
+                count--;
+                result = true;
+            }
         }
         return result;
     }
--- chapter_005/src/test/java/ru/job4j/SimpleHashMapTest.java
+++ chapter_005/src/test/java/ru/job4j/SimpleHashMapTest.java
@@ -20,35 +20,59 @@ public class SimpleHashMapTest {
 
     @Test
     public void whenInsertKeyAndValueThenHaveValueOnKey() {
-        simpleHashMap.insert("Прогер", "Игнат");
-        String result = simpleHashMap.get("Прогер");
+        simpleHashMap.insert("0", "Игнат");
+        String result = simpleHashMap.get("0");
         assertThat(result, is("Игнат"));
     }
 
     @Test
     public void whenInsertKeysAndValuesThenHaveValueOnKey() {
-        simpleHashMap.insert("Прогер", "Игнат");
-        simpleHashMap.insert("Инженер", "Влад");
-        simpleHashMap.insert("Админ", "Георг");
-        simpleHashMap.insert("Прогер", "Петр");
-        String result = simpleHashMap.get("Прогер");
+        simpleHashMap.insert("0", "Игнат");
+        simpleHashMap.insert("1", "Влад");
+        simpleHashMap.insert("2", "Георг");
+        simpleHashMap.insert("0", "Петр");
+        String result = simpleHashMap.get("0");
         assertThat(result, is("Петр"));
     }
 
     @Test
     public void whenDeleteKeyThenHaveNotValueOnKey() {
-        simpleHashMap.insert("Прогер", "Игнат");
-        simpleHashMap.delete("Прогер");
-        String result = simpleHashMap.get("Прогер");
+        simpleHashMap.insert("0", "Игнат");
+        simpleHashMap.delete("0");
+        String result = simpleHashMap.get("0");
         assertThat(result, is((String) null));
+    }
+
+
+    @Test
+    public void whenAddMore10ElToSmThenSmGrowUp() {
+        simpleHashMap.insert("0", "Игнат");
+        simpleHashMap.insert("1", "Влад");
+        simpleHashMap.insert("2", "Георг");
+        simpleHashMap.insert("3", "Петр");
+        simpleHashMap.insert("4", "Игнат");
+        simpleHashMap.insert("5", "Влад");
+        simpleHashMap.insert("6", "Георг");
+        simpleHashMap.insert("7", "Петр");
+        simpleHashMap.insert("8", "Влад");
+        simpleHashMap.insert("9", "Георг");
+        simpleHashMap.insert("10", "Петр");
+        simpleHashMap.insert("11", "Игнат");
+        simpleHashMap.insert("12", "Петр");
+        simpleHashMap.insert("13", "Влад");
+        simpleHashMap.insert("14", "Георг");
+        simpleHashMap.insert("15", "Петр");
+        simpleHashMap.insert("16", "Игнат");
+        String result = simpleHashMap.get("15");
+        assertThat(result, is("Петр"));
     }
 
     @Test(expected = NoSuchElementException.class)
     public void hasNextNextSequentialInvocationNoDupl() {
-        simpleHashMap.insert("Прогер", "Игнат");
-        simpleHashMap.insert("Инженер", "Влад");
-        simpleHashMap.insert("Админ", "Георг");
-        simpleHashMap.insert("Прогер", "Петр");
+        simpleHashMap.insert("0", "Игнат");
+        simpleHashMap.insert("1", "Влад");
+        simpleHashMap.insert("2", "Георг");
+        simpleHashMap.insert("0", "Петр");
         Iterator<String> it = simpleHashMap.iterator();
         String key = "";
         assertThat(it.hasNext(), is(true));
